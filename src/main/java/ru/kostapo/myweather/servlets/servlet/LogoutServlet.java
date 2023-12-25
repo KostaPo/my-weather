3b4b69917e2c5de6a2eb6b081cbe4011794be334
Raul Moralez <test@mail.ru>
2023-12-25 19:35:31 +0300

    fix web.xml

--- src/main/java/ru/kostapo/myweather/servlets/servlet/LogoutServlet.java
+++ src/main/java/ru/kostapo/myweather/servlets/servlet/LogoutServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet (name = "Logout", urlPatterns = "/logout")
+@WebServlet (name = "LogoutServlet", urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
 
     private AuthorizationService authService;
